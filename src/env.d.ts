72057074f66ab742050028f92d71cba5a1407e81
Bruno Germano <bruno.germano@gmail.com>
2025-07-15 18:05:43 -0300

    feat: add Azion Pulse analytics script and remove debug logs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  azpulse?: any; // You can replace 'any' with a more specific type if you know the shape of azpulse
+}
