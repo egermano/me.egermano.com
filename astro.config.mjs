16a3d25dd535b4a417e112ba9c6a027c0c7f19bf
Bruno Germano <bruno.germano@gmail.com>
2025-10-09 22:08:58 -0300

    feat: add umami tracker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import yeskunallumami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    yeskunallumami({
+      id: "c28a2a0a-d725-4ee0-81bd-605d439e3fb3",
+      endpointUrl: "https://umami.herbie-hancock.egermano.com",
+    }),
+  ],
+});
